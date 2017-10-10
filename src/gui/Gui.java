7d04c945740eb14e4c1b27359a5b9dc7a40fa511
Gustavo Pistore <thetunner@hotmail.com>
2017-10-10 20:24:41 -0300

    Criação Inicial da interface de usuário

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -1,5 +1,76 @@
 package gui;
 
-public class Gui {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 
+import javax.swing.*;
+
+
+public class Gui extends JFrame {
+	JFrame Janela = new JFrame();
+	JPanel painel;
+	
+	public Gui() {
+		super("Presa-Predador");
+		Janela.setMinimumSize(new Dimension(1024,768));
+		Janela.setMaximumSize(new Dimension(1024,768));
+		Janela.setResizable(false);
+		Janela.setTitle("Presa-Predador");
+		Janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+	}	
+	
+	public void desenhar(int[][] tabela) throws InterruptedException {
+		painel = new JPanel();
+		painel.setLayout(new GridLayout(tabela.length,tabela[0].length));
+		for (int i=0; i<tabela.length;i++) {
+			for(int j=0;j<tabela[0].length;j++) {
+				JPanel bloco = new JPanel();
+				/*
+				 VALOR		REPRESENTAÇÃO 
+				 0			Pixel livre (verde)
+				 1			Presa em modo normal (Branco)
+				 2			Presa no modo defensivo (Vermelho) 
+				 3			Predador (Cinza) 
+				  
+				 */
+				switch(tabela[i][j]) {
+					case 0:{
+						Color bg = new Color(124, 252, 0);
+						bloco.setBackground(bg);
+						break;
+					}
+					case 1:{
+						Color bg = new Color(240, 240, 240);
+						bloco.setBackground(bg);
+						break;
+					}
+					case 2:{
+						Color bg = new Color(255, 40, 40);
+						bloco.setBackground(bg);
+						break;
+					}
+					case 3:{
+						Color bg = new Color(100, 100, 100);
+						bloco.setBackground(bg);
+						break;
+					}
+					default: {
+						Color bg = new Color(124, 252, 0);
+						bloco.setBackground(bg);
+						break;
+					}
+				}
+				painel.add(bloco);
+				
+			}
+		}
+		Janela.add(painel, BorderLayout.CENTER);
+		Janela.pack();
+		Janela.setVisible(true);
+	}
+	
+	
+	
 }
--- src/main/main.java
+++ src/main/main.java
@@ -1,10 +1,23 @@
 package main;
 
-public class main {
+import java.util.Random;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import gui.Gui;
 
+public class Main {
+
+	public static void main(String[] args) throws InterruptedException {
+		Random gerador = new Random();
+		System.out.println("Hello World!");
+		//Teste de interface
+		int[][] tabela = new int[100][100];
+		for (int i=0;i<100;i++) {
+			for (int j=0;j<100;j++) {
+				tabela[i][j] = gerador.nextInt(4);
+			}
+		}
+		gui.Gui tela = new Gui();
+		tela.desenhar(tabela);
 	}
 
 }
